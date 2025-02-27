1adcfade5f172b8dab6df0d88da46836d294679a
JKandavel <163574871+JKandavel@users.noreply.github.com>
2025-02-27 12:18:27 +0530

    Update Abouthero.js

--- src/components/Abouthero.js
+++ src/components/Abouthero.js
@@ -6,7 +6,7 @@ export default function Abouthero() {
       <section>
         <Nav/>
         <div className="heroo">
-          <img src={More}/>
+          <img src={More} alt="image"/>
           <p>"Hi, I'm JK, a passionate front-end developer and a Computer Science Engineering student at Dhanalakshmi College of Engineering. I strive to build reliable and efficient web projects with a strong focus on user experience and functionality."</p>
         </div>
         <a className="resume" href="/">Click here:<span className="click">Resume</span></a>
